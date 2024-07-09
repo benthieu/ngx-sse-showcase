ba1f5508e95e15d3aa7d044237a1b5663952b800
Benjamin Mathieu (U119247) <benjamin.mathieu@mobi.ch>
2024-07-09 14:35:35 +0200

    showcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { HelloComponent } from './hello/hello.component';
+import { WorldComponent } from './world/world.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [AsyncPipe, HelloComponent, WorldComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'ngx-sse-showcase';
+export class AppComponent implements OnInit {
+  timer = interval(1000);
+  isready = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isready = true;
+    }, 1000);
+  }
 }
